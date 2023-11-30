464b544a65ca3e1e2b4c856f3aadbbbf373bdb40
Hoda Salah <37372736+hodasalah@users.noreply.github.com>
2023-11-30 22:18:54 +0300

    add server and payload and mongodb

---
+++ src/payload.config.ts
@@ -0,0 +1,42 @@
+import { buildConfig } from 'payload/config'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import path from 'path'
+
+
+
+
+export default buildConfig({
+  // By default, Payload will boot up normally
+  // and you will be provided with a base `User` collection.
+  // But, here is where you define how you'd like Payload to work!
+  serverURL:process.env.NEXT_PUBLIC_SERVER_URL || "",
+  collections:[],
+  // Configure the Mongoose adapter here
+  db: mongooseAdapter({
+    // Mongoose-specific arguments go here.
+    // URL is required.
+    url: process.env.MONGODB_URL ?process.env.MONGODB_URL: false ,
+  }),
+
+  routes:{
+    admin:"/sell",
+  },
+  admin:{
+    bundler: webpackBundler(),
+    meta:{
+      titleSuffix:"- DigitaMarkitting",
+      favicon:"/favicon.ico",
+      ogImage:"/thumbnail.jpg"
+    }
+  },
+  rateLimit:{
+    max:2000,
+  },
+  editor: slateEditor({}),
+  typescript:{
+    outputFile:path.resolve(__dirname,"payload-types.ts")
+  }
+
+})
